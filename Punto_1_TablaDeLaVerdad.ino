adc08899257b72db11fdee350acfe201973fd49d
DAPWPF <147282739+DAPWPF@users.noreply.github.com>
2023-10-27 07:37:07 -0500

    Update Punto_1_TablaDeLaVerdad.ino
    update

--- Punto_1_TablaDeLaVerdad.ino
+++ Punto_1_TablaDeLaVerdad.ino
@@ -1,87 +1,122 @@
 const int dipSwitch[] = {11, 10, 9, 8};
-const int outputLED1 = 7;  
-const int outputLED2 = 6;  
-const int outputLED3 = 5;  
+const int LED1 = 7;  
+const int LED2 = 6;  
+const int LED3 = 5;  
 
 void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(dipSwitch[i], INPUT);
   }
-  pinMode(outputLED1, OUTPUT);
-  pinMode(outputLED2, OUTPUT);
-  pinMode(outputLED3, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
   int switchState[4];
 
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 4; i++) 
+  {
     switchState[i] = digitalRead(dipSwitch[i]);
   }
 
-  if (switchState[0] == HIGH && switchState[1] == HIGH && switchState[2] == HIGH && switchState[3] == HIGH) {
-    digitalWrite(outputLED1, HIGH);
-    digitalWrite(outputLED2, HIGH);
-    digitalWrite(outputLED3, HIGH);
-  } else if (switchState[0] == HIGH && switchState[1] == HIGH && switchState[2] == HIGH && switchState[3] == LOW) {
-    digitalWrite(outputLED1, HIGH);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, HIGH);
-  } else if (switchState[0] == HIGH && switchState[1] == HIGH && switchState[2] == LOW && switchState[3] == HIGH) {
-    digitalWrite(outputLED1, HIGH);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, HIGH);
-  } else if (switchState[0] == HIGH && switchState[1] == HIGH && switchState[2] == LOW && switchState[3] == LOW) {
-    digitalWrite(outputLED1, LOW);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, HIGH);
-  } else if (switchState[0] == HIGH && switchState[1] == LOW && switchState[2] == HIGH && switchState[3] == HIGH) {
-    digitalWrite(outputLED1, HIGH);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, HIGH);
-  }else if (switchState[0] == HIGH && switchState[1] == LOW && switchState[2] == HIGH && switchState[3] == LOW) {
-    digitalWrite(outputLED1, LOW);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, HIGH);
-  }else if (switchState[0] == HIGH && switchState[1] == LOW && switchState[2] == LOW && switchState[3] == HIGH) {
-    digitalWrite(outputLED1, LOW);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, HIGH);
-  }else if (switchState[0] == HIGH && switchState[1] == LOW && switchState[2] == LOW && switchState[3] == LOW) {
-    digitalWrite(outputLED1, LOW);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, LOW);
-  }else if (switchState[0] == LOW && switchState[1] == HIGH && switchState[2] == HIGH && switchState[3] == HIGH) {
-    digitalWrite(outputLED1, HIGH);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, HIGH);
-  }else if (switchState[0] == LOW && switchState[1] == HIGH && switchState[2] == HIGH && switchState[3] == LOW) {
-    digitalWrite(outputLED1, LOW);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, HIGH);
-  }else if (switchState[0] == LOW && switchState[1] == HIGH && switchState[2] == LOW && switchState[3] == HIGH) {
-    digitalWrite(outputLED1, LOW);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, HIGH);
-  }else if (switchState[0] == LOW && switchState[1] == HIGH && switchState[2] == LOW && switchState[3] == LOW) {
-    digitalWrite(outputLED1, HIGH);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, LOW);
-  }else if (switchState[0] == LOW && switchState[1] == LOW && switchState[2] == HIGH && switchState[3] == HIGH) {
-    digitalWrite(outputLED1, LOW);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, HIGH);
-  }else if (switchState[0] == LOW && switchState[1] == LOW && switchState[2] == HIGH && switchState[3] == LOW) {
-    digitalWrite(outputLED1, HIGH);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, LOW);
-  }else if (switchState[0] == LOW && switchState[1] == LOW && switchState[2] == LOW && switchState[3] == HIGH) {
-    digitalWrite(outputLED1, HIGH);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, LOW);
-  }else {
-    digitalWrite(outputLED1, LOW);
-    digitalWrite(outputLED2, LOW);
-    digitalWrite(outputLED3, LOW);
+
+  if (switchState[3] == HIGH && switchState[2] == HIGH && switchState[1] == LOW && switchState[0] == LOW) 
+  {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+  }
+  else if (switchState[3] == HIGH && switchState[2] == HIGH && switchState[1] == LOW && switchState[0] == HIGH)
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+  }
+  else if (switchState[3] == HIGH && switchState[2] == HIGH && switchState[1] == HIGH && switchState[0] == LOW)
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+  }
+  else if (switchState[3] == HIGH && switchState[2] == HIGH && switchState[1] == HIGH && switchState[0] == HIGH)
+  {
+    digitalWrite (LED1, LOW);
+    digitalWrite (LED2, LOW);
+    digitalWrite (LED3, HIGH);
+  } 
+  else if (switchState[3] == HIGH && switchState[2] == LOW && switchState[1] == LOW && switchState[0] == LOW)
+  {
+    digitalWrite (LED1, HIGH);
+    digitalWrite (LED2, LOW);
+    digitalWrite (LED3, LOW);
+  } 
+  else if (switchState[3] == HIGH && switchState[2] == LOW && switchState[1] == LOW && switchState[0] == HIGH)
+  {
+    digitalWrite (LED1, LOW);
+    digitalWrite (LED2, LOW);
+    digitalWrite (LED3, HIGH);
+  } 
+  else if (switchState[3] == HIGH && switchState[2] == LOW && switchState[1] == HIGH && switchState[0] == LOW) 
+  {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+  }
+  else if (switchState[3] == HIGH && switchState[2] == LOW && switchState[1] == HIGH && switchState[0] == HIGH) 
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+  }
+  else if (switchState[3] == LOW && switchState[2] == HIGH && switchState[1] == LOW && switchState[0] == LOW) 
+  {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+  }
+  else if (switchState[3] == LOW && switchState[2] == HIGH && switchState[1] == LOW && switchState[0] == HIGH) 
+  {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+  }
+  else if (switchState[3] == LOW && switchState[2] == HIGH && switchState[1] == HIGH && switchState[0] == LOW) 
+  {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+  }
+  else if (switchState[3] == LOW && switchState[2] == HIGH && switchState[1] == HIGH && switchState[0] == HIGH) 
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+  }
+  else if (switchState[3] == LOW && switchState[2] == LOW && switchState[1] == LOW && switchState[0] == LOW) 
+  {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+  }
+   else if (switchState[3] == LOW && switchState[2] == LOW && switchState[1] == LOW && switchState[0] == HIGH) 
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+  }
+   else if (switchState[3] == LOW && switchState[2] == LOW && switchState[1] == HIGH && switchState[0] == LOW) 
+  {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+  }
+  
+   else if (switchState[3] == LOW && switchState[2] == LOW && switchState[1] == HIGH && switchState[0] == HIGH) 
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
   }
 }
