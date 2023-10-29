689f2e54189188508539f9bb609d3ca5291091c8
DAPWPF <147282739+DAPWPF@users.noreply.github.com>
2023-10-29 12:37:10 -0500

    Update Punto_4_Semaforo2.ino
    me la pelan

--- Punto_4_Semaforo2.ino
+++ Punto_4_Semaforo2.ino
@@ -3,7 +3,8 @@
 #define LEDverde 5
 #define POTENCIOMETRO A0
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   pinMode(11, INPUT);
   pinMode(10, INPUT);
@@ -13,46 +14,50 @@ void setup() {
   pinMode(LEDverde, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
   bool swi1, swi2;
   swi1 = digitalRead(11);
   swi2 = digitalRead(10);
-int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
+int sensor = analogRead(A0);
+   int velocidad = map (sensor,0,1023,300,50);
+  Serial.print("se cumplio un ciclo");
+  Serial.println(".");
   delay(1);
-  int potValue = analogRead(POTENCIOMETRO);
-
-
-  if (swi1 == 1 && swi2 == 0) {
+  
+  if (swi1 == 1 && swi2 == 0) 
+  {
      digitalWrite(LEDrojo, HIGH);
       digitalWrite(LEDamarillo, LOW);
       digitalWrite(LEDverde, LOW);
-      delay(sensorValue);
+      delay(velocidad*10);
 
       digitalWrite(LEDrojo, HIGH);
       digitalWrite(LEDamarillo, HIGH);
       digitalWrite(LEDverde, LOW);
-      delay(sensorValue*1000);
+      delay(velocidad*10);
 
       digitalWrite(LEDrojo, LOW);
       digitalWrite(LEDamarillo, LOW);
       digitalWrite(LEDverde, HIGH);
-      delay(sensorValue*3000);
+      delay(velocidad*10);
 
       digitalWrite(LEDrojo, LOW);
       digitalWrite(LEDverde, LOW);
       digitalWrite(LEDamarillo, HIGH);
-      delay(sensorValue*1000);
+      delay(velocidad*10);
   }
-  if(swi1==0 && swi2==1){
+  if(swi1==0 && swi2==1)
+  {
    digitalWrite(LEDrojo, LOW);
    digitalWrite(LEDverde, LOW);
     digitalWrite(LEDamarillo, HIGH);
-      delay(sensorValue*500);
+      delay(500);
       digitalWrite(LEDamarillo, LOW);
-      delay(sensorValue*500);
+      delay(500);
   }
-  else{
+  else
+  {
    digitalWrite(LEDrojo, LOW);
    digitalWrite(LEDverde, LOW);
    digitalWrite(LEDamarillo,LOW);
