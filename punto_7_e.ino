471c75f2874a23631a2bf65e91c6cf92f4305f4a
MiguelSabogal <148549781+ELsarcoaletoso@users.noreply.github.com>
2023-11-03 09:04:13 -0500

    Add files via upload

---
+++ punto_7_e.ino
@@ -0,0 +1,55 @@
+#define POTENCIOMETRO A0
+#define LED_AZUL D7
+#define LED_ROJO D8
+#define LED_INTERNO D4
+
+void setup() {
+ pinMode(LED_AZUL, OUTPUT);
+ pinMode(LED_ROJO, OUTPUT);
+ pinMode(LED_INTERNO, OUTPUT);
+ pinMode(POTENCIOMETRO, INPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+ int sensorValue = analogRead(POTENCIOMETRO);
+ Serial.println(sensorValue); 
+
+ int mitad = 541;
+ int maximo = 1000;
+ int minimo = 25;
+
+ if (sensorValue <= minimo) {
+   digitalWrite(LED_AZUL, HIGH);
+   digitalWrite(LED_ROJO, HIGH);
+   digitalWrite(LED_INTERNO, LOW);
+   delay(500);
+   digitalWrite(LED_AZUL, LOW);
+   digitalWrite(LED_ROJO, LOW);
+   digitalWrite(LED_INTERNO, HIGH);
+   delay(500);
+ }
+ if (sensorValue >= mitad) {
+   digitalWrite(LED_ROJO, HIGH);
+ } else {
+   digitalWrite(LED_ROJO, LOW);
+ }
+ 
+ if (sensorValue <= mitad && sensorValue > minimo) {
+   digitalWrite(LED_INTERNO, HIGH);
+   delay(500);
+   digitalWrite(LED_INTERNO, LOW);
+   delay(500);
+   digitalWrite(LED_AZUL, LOW);
+   } else {
+   digitalWrite(LED_ROJO, LOW);
+ }
+ if (sensorValue <= maximo) {
+    digitalWrite(LED_ROJO, HIGH);
+    digitalWrite(LED_AZUL,LOW);
+   
+ } else {
+   digitalWrite(LED_AZUL, HIGH);
+ }
+
+}
