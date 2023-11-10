274b17875f46aa51dbea2459f275a4b0fb87823c
MiguelSabogal <148549781+ELsarcoaletoso@users.noreply.github.com>
2023-11-10 08:07:18 -0500

    Add files via upload

---
+++ punto_9_Sensor_de_humedad.ino
@@ -0,0 +1,38 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include <DHT.h>
+#define DHTPIN D4
+#define DHTTYPE DHT11
+ 
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() 
+{
+  Serial.begin(115200);
+  dht.begin();
+}
+ 
+void loop() 
+{
+    delay(5000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+ 
+  
+  if (isnan(h) || isnan(t)) 
+  {
+    Serial.println("error obteniendo los datos del sensor DHT11");
+    return;
+  }
+  float hic = dht.computeHeatIndex(t, h, false);
+ 
+  Serial.print("humedad: ");
+  Serial.print(h);
+  Serial.println(" % \t");
+  Serial.print("temperatura: ");
+  Serial.print(t);
+  Serial.println(" °C\t");  
+}
