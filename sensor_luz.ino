850d217317dc331a65a35b79141f5f7cbf70db64
DAPWPF <147282739+DAPWPF@users.noreply.github.com>
2023-11-29 17:32:22 -0500

    Add files via upload

---
+++ sensor_luz.ino
@@ -0,0 +1,52 @@
+
+#include <DHT.h>
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+float h;
+int wPin=7;
+int lightPin = A0;
+int lightVal;
+int t = 500;
+int motor = 10;
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(lightPin, INPUT);
+  pinMode(wPin, OUTPUT);
+  pinMode(MOTOR, OUTPUT);
+}
+
+void loop()
+{
+ 
+  lightVal = analogRead(lightPin);
+  Serial.println(lightVal);
+  delay(t);
+
+  if(lightVal >700){
+    digitalWrite(wPin, HIGH);
+  }
+  if(lightVal <= 700){
+
+    digitalWrite(wPin, LOW);
+  }
+  {
+     delay(2000);
+
+  h = dht.readHumidity();
+  if (isnan(h)) 
+  {
+    Serial.println("error obteniendo los datos del sensor DHT11");
+    return;
+  }
+  float hic = dht.computeHeatIndex(h, false);
+ 
+  Serial.print("humedad: ");
+  Serial.print(h);
+  Serial.println(" % \t");
+  }
+}
+
