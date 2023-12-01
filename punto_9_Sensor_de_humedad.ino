423d5808cc84a4d7dd7ed5f6ecde1451f431a12a
MiguelSabogal <148549781+ELsarcoaletoso@users.noreply.github.com>
2023-12-01 12:05:34 -0500

    Update punto_9_Sensor_de_humedad.ino

--- punto_9_Sensor_de_humedad.ino
+++ punto_9_Sensor_de_humedad.ino
@@ -1,63 +1,35 @@
-
-
-int waterVal;
-int wPin=11;
-int lightPin = A0;
-int waterPin = A1;
-int lightVal;
-int motor1 = 8;
-int vel =10;
-int pwm ;
-float ref = 500.0;
-int error;
-int k = 1;
+#include <DHT.h>
+#include <DHT_U.h>
+#include <DHT.h>
+#define DHTPIN D4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+float h;
+float t;
 void setup() 
 {
-  Serial.begin(9600);
-  pinMode(lightPin, INPUT);
-  pinMode(wPin, OUTPUT);
-  pinMode(motor1, OUTPUT);
-  pinMode(motor1, OUTPUT);
-  pinMode(vel, OUTPUT);
+  Serial.begin(115200);
+  dht.begin();
 }
-
-void loop()
-{
  
-  lightVal = analogRead(lightPin);                                                      
-  Serial.print(lightVal);
-   Serial.println("luz");
-  delay(500);
+void loop() 
+{
+    delay(5000);
 
-  delay(100);
+  h = dht.readHumidity();
+  t = dht.readTemperature();
 
-  if(lightVal >700)
+  if (isnan(h) || isnan(t)) 
   {
-    digitalWrite(wPin, HIGH);
+    Serial.println("error obteniendo los datos del sensor DHT11");
+    return;
   }
-  if(lightVal <= 700)
-  {
-    digitalWrite(wPin, LOW);
-  }
-
-   waterVal = analogRead(waterPin);                                                      
-  Serial.print(waterVal);
-  Serial.println("agua");
-  delay(500);
-  
-  if ( waterVal>=1000)
-  {
-  Serial.println("error obteniendo los datos del sensor");
-  } 
-  error =ref-waterVal;
-  pwm =k*error;
-  
-    analogWrite(vel,pwm); 
-    digitalWrite(motor1, HIGH);
-    delay(2000);
-
-  if (pwm>225)
-  { 
-   pwm =225;
-  }
+  float hic = dht.computeHeatIndex(t, h, false);
+ 
+  Serial.print("humedad: ");
+  Serial.print(h);
+  Serial.println(" % \t");
+  Serial.print("temperatura: ");
+  Serial.print(t);
+  Serial.println(" °C\t");  
 }
